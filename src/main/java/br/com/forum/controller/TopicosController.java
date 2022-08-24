14ca8b74ab37caf9e58e836f3a7cc4b28a23c0af
Lucas Gabriel Coelho Vieira <vieraf1@hotmail.com.br>
2022-08-23 22:18:53 -0300

    Modificando o status de retorno do rest de criação de tópico
    Modificando o status de retorno do rest de criação de tópico

--- src/main/java/br/com/forum/controller/TopicosController.java
+++ src/main/java/br/com/forum/controller/TopicosController.java
@@ -1,16 +1,20 @@
 package br.com.forum.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.forum.controller.form.TopicoForm;
 import br.com.forum.dto.TopicoDto;
+import br.com.forum.modelo.Topico;
 import br.com.forum.repository.CursoRepository;
 import br.com.forum.repository.TopicoRepository;
 
@@ -35,9 +39,12 @@ public class TopicosController {
 	}
 	
 	@PostMapping
-	public void cadastrar(@RequestBody TopicoForm topicoForm) {
-		topicoRepository.save(
-				topicoForm.convertToTopico(cursoRepository));
+	public ResponseEntity<TopicoDto> cadastrar(@RequestBody TopicoForm topicoForm, UriComponentsBuilder uriBuilder) {
+		Topico topico = topicoForm.convertToTopico(cursoRepository);
+		topicoRepository.save(topico);
+		
+		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+		return ResponseEntity.created(uri).body(new TopicoDto(topico));
 	}
 
 }
