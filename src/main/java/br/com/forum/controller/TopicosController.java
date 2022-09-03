a036811d8d846c35aa287859fc99444102d920ca
Lucas Gabriel Coelho Vieira <vieraf1@hotmail.com.br>
2022-09-02 22:36:48 -0300

    Detalhando as informações retornadas do Topico
    Detalhando as informações retornadas do Topico

--- src/main/java/br/com/forum/controller/TopicosController.java
+++ src/main/java/br/com/forum/controller/TopicosController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.forum.controller.form.TopicoForm;
+import br.com.forum.dto.DetalheTopicoDto;
 import br.com.forum.dto.TopicoDto;
 import br.com.forum.modelo.Topico;
 import br.com.forum.repository.CursoRepository;
@@ -47,6 +49,13 @@ public class TopicosController {
 		
 		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
 		return ResponseEntity.created(uri).body(new TopicoDto(topico));
+	}
+	
+	@GetMapping("/{id}")
+	public DetalheTopicoDto detalhar(@PathVariable Long id) {
+		Topico topico = topicoRepository.getReferenceById(id);
+		
+		return new DetalheTopicoDto(topico);
 	}
 
 }
---
+++ src/main/java/br/com/forum/dto/DetalheTopicoDto.java
@@ -0,0 +1,58 @@
+package br.com.forum.dto;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import br.com.forum.modelo.Topico;
+import br.com.forum.modelo.enums.StatusTopico;
+
+public class DetalheTopicoDto {
+
+	private Long id;
+	private String titulo;
+	private String mensagem;
+	private LocalDateTime dataCriacao;
+	private String nomeAutor;
+	private StatusTopico status;
+	private List<RespostaDto> repostas = new ArrayList<>();
+	
+	public DetalheTopicoDto(Topico topico) {
+		this.id = topico.getId();
+		this.titulo = topico.getTitulo();
+		this.mensagem = topico.getMensagem();
+		this.dataCriacao = topico.getDataCriacao();
+		this.nomeAutor = topico.getAutor().getNome();
+		this.status = topico.getStatus();
+		this.repostas.addAll(topico.getRespostas().stream().map(RespostaDto::new).collect(Collectors.toList()));
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public LocalDateTime getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public String getNomeAutor() {
+		return nomeAutor;
+	}
+
+	public StatusTopico getStatus() {
+		return status;
+	}
+
+	public List<RespostaDto> getRepostas() {
+		return repostas;
+	}
+}
