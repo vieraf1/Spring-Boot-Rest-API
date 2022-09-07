e403c98b62f5d148b02b555495a9d270cfc077d2
Lucas Gabriel Coelho Vieira <vieraf1@hotmail.com.br>
2022-09-06 22:35:15 -0300

    Aplicando ordenação
    Aplicando ordenação

--- src/main/java/br/com/forum/controller/TopicosController.java
+++ src/main/java/br/com/forum/controller/TopicosController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -42,9 +43,9 @@ public class TopicosController {
 	
 	@GetMapping
 	public Page<TopicoDto> listar(@RequestParam(required = false) String nomeCurso, @RequestParam int pagina
-			, @RequestParam int quantidadePorPagina) {
+			, @RequestParam int quantidadePorPagina, @RequestParam String ordenacao) {
 		
-		Pageable paginacao = PageRequest.of(pagina, quantidadePorPagina);
+		Pageable paginacao = PageRequest.of(pagina, quantidadePorPagina, Direction.DESC, ordenacao);
 		
 		if(nomeCurso != null) {
 			return TopicoDto.convertFromTopico(
