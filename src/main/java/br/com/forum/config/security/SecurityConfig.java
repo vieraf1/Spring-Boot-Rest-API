a6446993778396f9e96b3cd3789d5964ddf34163
Lucas Gabriel Coelho Vieira <vieraf1@hotmail.com.br>
2022-09-19 22:40:23 -0300

    Criando a autenticação via token dos serviços
    Criando a autenticação via token dos serviços

--- src/main/java/br/com/forum/config/security/SecurityConfig.java
+++ src/main/java/br/com/forum/config/security/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @SuppressWarnings("deprecation")
 @EnableWebSecurity
@@ -42,7 +43,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	    .antMatchers(HttpMethod.POST, "/auth").permitAll()
 	    .anyRequest().authenticated()
 	    .and().csrf().disable()
-	    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+	    .and().addFilterBefore(new AutenticacaoViaTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
 	
 	//Configurações de recursos estáticos(js, imagens, css, etc.)
