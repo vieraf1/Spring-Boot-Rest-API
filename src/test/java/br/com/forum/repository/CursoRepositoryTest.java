d6a9ba3120aac1c336d7e708391601243c861243
Lucas Gabriel Coelho Vieira <vieraf1@hotmail.com.br>
2022-10-17 22:25:28 -0300

    Realizando o teste usando um banco diferente, utilizando profile para separar os bancos dos ambientes
    Realizando o teste usando um banco diferente, utilizando profile para separar os bancos dos ambientes

--- src/test/java/br/com/forum/repository/CursoRepositoryTest.java
+++ src/test/java/br/com/forum/repository/CursoRepositoryTest.java
@@ -1,21 +1,36 @@
 package br.com.forum.repository;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.junit.Assert;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 
 import br.com.forum.modelo.Curso;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
 public class CursoRepositoryTest {
 
 	@Autowired
 	private CursoRepository repository;
 	
+	@Autowired
+	private TestEntityManager em;
+	
 	@Test
 	public void deveriaCarregarCursoPeloNome() {
 		String nomeCurso = "HTML 5";	
+		
+		Curso html5 = new Curso();
+		html5.setNome(nomeCurso);
+		html5.setCategoria("Programação");
+		
+		em.persist(html5);
+		
 		Curso curso = repository.findByNome(nomeCurso);
 		
 		Assert.assertNotNull(curso);
